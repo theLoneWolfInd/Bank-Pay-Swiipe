8094c7fa2aa830b51e76e75fcc4d91b6d42a3289
Dishant Rajput <dishantrajput2020@gmail.com>
2019-10-25 18:29:32 +0530

    Initial Commit

---
+++ Swipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swipe
+//
+//  Created by Apple  on 25/10/19.
+//  Copyright © 2019 Apple . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
